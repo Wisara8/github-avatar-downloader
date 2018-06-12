62f29f9b48984d2e01da155869b997de28495537
Wisara8 <srandall845@gmail.com>
2018-06-12 00:41:40 +0000

    downloader working with hard code

--- download_avatar.js
+++ download_avatar.js
@@ -1,29 +1,46 @@
 var request = require('request');
-// var fs = require('fs');
+var fs = require('fs');
 var token = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': token
-    }
-  };
+// function getRepoContributors(repoOwner, repoName, cb) {
+//   var options = {
+//     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+//     headers: {
+//       'User-Agent': 'request',
+//       'Authorization': token
+//     }
+//   };
 
+//   request(options, function(err, res, body) {
+//     var result = JSON.parse(body);
 
-  request(options, function(err, res, body) {
-    var result = JSON.parse(body);
+//     cb(err, result);
+//     for (var i = 0; i < result.length; i++) {
+//       console.log(result[i].avatar_url);
+//     }
+//   });
+// }
 
-    cb(err, result);
-    for (var i = 0; i < result.length; i++) {
-      console.log(result[i].avatar_url);
-    }
-  });
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+  .on('error', function(err) {
+    throw err;
+  })
+
+  .on('response', function (response) {
+    console.log('Downloading image...');
+  })
+  .on('end', function(){
+    console.log('Download complete.');
+  })
+  .pipe(fs.createWriteStream(filePath));
 }
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  // console.log("Result:", result);
-});
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "kvirani.jpg");
+
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log("Errors:", err);
+//   // console.log("Result:", result);
+// });
